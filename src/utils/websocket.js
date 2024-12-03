8e8bc8561611bb66c1750dc753c181193b0ba4ba
Marcos Amorim <126255748+mkdev-2@users.noreply.github.com>
2024-12-03 16:12:47 -0300

    modulacao v8

--- src/utils/websocket.js
+++ src/utils/websocket.js
@@ -1,7 +1,7 @@
 
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.VITE_API_URL || 'http://localhost:3000', {
+const socket = io(process.env.VITE_API_URL, {
   withCredentials: true,
   transports: ['websocket'],
 });
