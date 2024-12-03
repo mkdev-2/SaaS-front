5a959f1ec44940356ae30ce2853d56cd1d7557a4
Marcos Amorim <126255748+mkdev-2@users.noreply.github.com>
2024-12-03 16:57:41 -0300

    modulacao v12

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,13 +4,20 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'https://saas-backend-production-8b94.up.railway.app',
 });
 
-// Adiciona o token ao cabeçalho de cada requisição
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken'); // Certifique-se de que o token está salvo no LocalStorage
+  const token = localStorage.getItem('accessToken');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+});
+
+api.interceptors.response.use((response) => {
+  const newToken = response.headers['x-new-token'];
+  if (newToken) {
+    localStorage.setItem('accessToken', newToken);
+  }
+  return response;
 });
 
 export default api;
