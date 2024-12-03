c2d7f441c6b5857714fcd802e6007fe041aba47e
Marcos Amorim <126255748+mkdev-2@users.noreply.github.com>
2024-12-03 16:43:56 -0300

    modulacao v10

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,16 @@
-
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'https://saas-backend-production-8b94.up.railway.app',
+});
+
+// Adiciona o token ao cabeçalho de cada requisição
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken'); // Certifique-se de que o token está salvo no LocalStorage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
